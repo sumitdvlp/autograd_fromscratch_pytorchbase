e3201399e3bd113afb467022d99aeaa719d4e5ba
Sumit Kumar <sumitdvlp@gmail.com>
2025-05-19 15:17:22 -0700

    added nn layer

---
+++ autograd_engine/nn_modules/layers.py
@@ -0,0 +1,50 @@
+from ..core import *
+from ..utils import *
+
+
+class Module:
+    ''' General Module superclass'''
+    def __init__(self):
+        pass
+
+    def __call__(self, x):
+        return self.forward(x)
+
+    def parameters(self):
+        '''
+        Returns all model parameters in a list. Iterates over each item in self.__dict__,
+        and returns every Parameter object, or Tensor objects with requires_grad set to True.
+
+        @returns params (list): All parameters in the model.
+        '''
+        params = []
+        for _, param in self.__dict__.items():
+            if isinstance(param, Module):
+                params += param.parameters()
+            elif isinstance(param, Parameter):
+                params.append(param)
+            elif isinstance(param, Tensor):
+                if param.requires_grad:
+                    params.append(param)
+        return params
+
+    def train(self):
+        ''' Sets module's mode to train, which influences layers like Dropout'''
+        self.mode = 'train'
+        for _, param in self.__dict__.items():
+            if isinstance(param, Module):
+                param.train()
+
+
+    def eval(self):
+        ''' Sets module's mode to eval, which influences layers like Dropout'''
+        self.mode = 'eval'
+        for _, param in self.__dict__.items():
+            if isinstance(param, Module):
+                param.eval()
+
+# Parameter subclass, inherits from Tensor:
+class Parameter(Tensor):
+    ''' Subclass of Tensor which always tracks gradients. '''
+    def __init__(self, data, requires_grad = True, operation = None,op='') -> None:
+        super().__init__(data, requires_grad=requires_grad, operation=operation,op=op)
